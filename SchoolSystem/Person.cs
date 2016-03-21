974b8ee8d2f2cbae7c9c86d617912685d1dd6f52
Matthew Xuereb <matxuereb@hotmail.co.uk>
2016-03-21 21:15:53 +0100

    Added person and student

---
+++ SchoolSystem/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem
+{
+    class Person
+    {
+        int id;
+        string surn;
+        float pay;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Surn
+        {
+            get { return surn; }
+            set { surn = value; }
+
+        }
+
+        public float Pay
+        {
+            get { return pay; }
+            set { pay = value; }
+        }
+
+        public Person()
+        { }
+
+        public Person(int iId, string iSurn, float iPay)
+        {
+            id = iId;
+            surn = iSurn;
+            pay = iPay;
+        }
+
+        public virtual string GetData()
+        {
+            return "ID: " + id + ", Surname: " + surn + ", Salary: " + GetSalary();
+        }
+
+        public virtual float GetSalary()
+        {
+            return pay;
+        }
+
+        /*        void PutSalary(float newPay)
+                {
+                    pay = newPay;
+                }
+                */
+    }
+}
---
+++ SchoolSystem/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolSystem
+{
+    class Student : Person
+    {
+        int[] marks = new int[4];
+
+        String GetMarks()
+        {
+            //Console.WriteLine(string.Join(", ", marks));
+            /*string temp = "";
+            for (int i = 0; i < marks.Length; i++)
+                temp = temp + marks[i] + ",";
+                */
+            //return temp;
+            return string.Join(", ", marks);
+        }
+        public Student(int iId, string iSurn, float iPay)
+            : base(iId, iSurn, iPay)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Enter mark {0}: ", i + 1);
+                string temp = Console.ReadLine();
+                marks[i] = Int32.Parse(temp);
+            }
+        }
+
+        public override string GetData()
+        {
+            return "Student:\n\tMarks: [" + GetMarks() + "], " + base.GetData();
+        }
+
+        public override float GetSalary()
+        {
+            float avg = 0;
+            for (int i = 0; i < marks.Length; i++)
+                avg += marks[i];
+            avg = avg / marks.Length;
+
+            if (avg >= 50)
+                return Pay;
+            else
+                return 0;
+        }
+    }
+}
